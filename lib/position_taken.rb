ba6dc5da53d9b3e23fde7959d8c02f9d47bc10f3
Leroy Hutchinson <l.hutchinsonj@gmail.com>
2018-06-20 20:36:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,13 +2,10 @@
 
 def position_taken?(board, index)
 
-
 if(board[index] == "" || board[index] == " " || board[index] == nil)
 
 false
 elsif board[index] == "X" || board[index] == "O"
 true
 end
-
-
 end
